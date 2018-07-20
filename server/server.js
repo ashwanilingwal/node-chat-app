89adc1860ae806164a2e58a14d0e02846405aa08
ashwanilingwal <ashwanicodelingwal1998@gmail.com>
2018-07-20 16:48:40 +0530

    init

--- server/server.js
+++ server/server.js
@@ -2,9 +2,10 @@ const path=require('path')
 const publicPath=path.join(__dirname+'/../public');
 const express=require('express');
 var app=express();
+const port=process.env.PORT||3000;
 
 
 app.use(express.static(publicPath));
-app.listen(3000,()=>{
-  console.log("Setting up the local host server");
+app.listen(port,()=>{
+    console.log(`Server is up on ${port}`);
 })
